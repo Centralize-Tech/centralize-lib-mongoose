a4b28bd4e7f69c9d3246472ca66b2bb7121bbd1f
unknown <mg.rodriguez.barrios@gmail.com>
2025-06-20 13:02:21 -0400

    Centralize - update enterprise

--- build/src/models/Centralize/EnterpriseCentralize.js
+++ build/src/models/Centralize/EnterpriseCentralize.js
@@ -4,13 +4,13 @@ exports.enterpriseCentralizeSchema = void 0;
 exports.createEnterpriseCentralizeModel = createEnterpriseCentralizeModel;
 const mongoose_1 = require("mongoose");
 exports.enterpriseCentralizeSchema = new mongoose_1.Schema({
-    name: { type: String, required: false },
+    name: { type: String, required: true },
     lastName: { type: String, required: false },
     email: { type: String, required: true },
-    phone: { type: String, required: true },
+    phone: { type: String, required: false },
     rut: { type: String, required: true },
     description: { type: String, required: false },
-    companyName: { type: String, required: false },
+    companyName: { type: String, required: true, unique: true },
     marketplace: { type: String, required: false },
     status: { type: Boolean, required: true, default: true },
 }, { timestamps: true });
--- src/models/Centralize/EnterpriseCentralize.ts
+++ src/models/Centralize/EnterpriseCentralize.ts
@@ -3,13 +3,13 @@ import { Schema, Connection } from 'mongoose';
 import { IEnterpriseCentralize } from '../../types/Centralize/IEnterpriseCentralize';
 
 export const enterpriseCentralizeSchema = new Schema<IEnterpriseCentralize>({
-  name: { type: String, required: false },
+  name: { type: String, required: true },
   lastName: { type: String, required: false },
   email: { type: String, required: true },
-  phone: { type: String, required: true },
+  phone: { type: String, required: false },
   rut: { type: String, required: true },
   description: { type: String, required: false },
-  companyName: { type: String, required: false },
+  companyName: { type: String, required: true, unique: true },
   marketplace: { type: String, required: false },
   status: { type: Boolean, required: true, default: true },
 }, { timestamps: true });

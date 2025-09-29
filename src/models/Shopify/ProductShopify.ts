42c49e09e8678662bfd888ef3e125a396dacf378
devHugoFica <hfica@centralize.cl>
2025-09-29 15:34:06 -0300

    feat: modificación en tipos de productos

--- src/models/Shopify/ProductShopify.ts
+++ src/models/Shopify/ProductShopify.ts
@@ -3,19 +3,22 @@ import { IProductType } from '../../types'
 
 export const productShopifySchema = new Schema<IProductType>(
   {
-    id: { type: String, required: true },
-    title: { type: String, required: true },
+    id: { type: Number, required: true },
+    created_at: { type: Date, required: true },
     handle: { type: String, required: true },
-    description: { type: String, required: true },
-    status: { type: String, required: true },
+    product_type: { type: String, required: true },
+    published_at: { type: Date, required: true },
+    title: { type: String, required: true },
+    updated_at: { type: Date, required: true },
     vendor: { type: String, required: true },
-    productType: { type: String, required: true },
+    status: { type: String, required: true },
+    published_scope: { type: String, required: true },
     tags: { type: String, required: true },
-    totalInventory: { type: Number, required: true },
-    createdAt: { type: Date, required: true },
-    updatedAt: { type: Date, required: true },
+    variants: [{ type: Object, required: true }],
+    options: [{ type: Object, required: true }],
     images: [{ type: Object, required: true }],
-    variants: [{ type: Object, required: true }]
+    image: { type: Object, required: true },
+    category: { type: Object, required: true }
   },
   { timestamps: true }
 )
